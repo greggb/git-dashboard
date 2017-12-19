22ff9da0738b0965b3c2ccdf94ce77a1a305dde2
Gregg <mail@digitalpuddle.com>
2017-12-18 20:53:47 -0500

    show branch list

--- src/index.js
+++ src/index.js
@@ -1,52 +1,93 @@
 const blessed = require('blessed');
-const getRepoInfo = require('git-repo-info');
+const repositoryInfo = require('git-info');
+const isEqual = require('lodash.isequal');
 
+const POLL_INTERVAL = 500;
+// git-info API: https://github.com/michalbe/git-info#api
+const API_FIELDS = [
+  'author', // top author of the repo
+  'authors', // list of all the authors
+  'authorDate', // date of last commit
+  'authorDateRelative', // date of last commit
+  'name', // name of the repository
+  'repository', // address of the repo
+  'branch', // current branch
+  'branches', // all the branches in the repo
+  'sha', // the sha of the last commit
+  'shaShort', // the sha of the last commit but in the short form
+  'subject', // the message of the last commit
+];
 
 class Dashboard {
   constructor() {
     const title = 'Git Dashboard';
     // Store info for this repo
     this.repoInfo = null;
-
     // initialize Blessed
     this.screen = blessed.screen({
       title,
       smartCSR: true,
       fullUnicode: true,
     });
-    //  Populate data
-    this.getData();
-    console.log(this.repoInfo);
+    this.render = this.render.bind(this);
+    this.addEventHandlers = this.addEventHandlers.bind(this);
+    this.layout = this.layout.bind(this);
+    this.getRepoInfo = this.getRepoInfo.bind(this);
+    this.poll = this.poll.bind(this);
+    this.updateRepoInfo = this.updateRepoInfo.bind(this);
+    this.hasChanged = this.hasChanged.bind(this);
 
-    // add panel
-    this.layout();
-    // Events
     this.addEventHandlers();
+    this.getRepoInfo(this.updateRepoInfo);
+  }
+  // eslint-disable-next-line
+    getRepoInfo(cb) {
+    repositoryInfo(API_FIELDS, cb);
+  }
+  render() {
     // GO
     this.screen.render();
   }
-  getData() {
-    this.repoInfo = getRepoInfo();
+  poll() {
+    setTimeout(() => {
+      this.getRepoInfo(this.updateRepoInfo);
+    }, POLL_INTERVAL);
+  }
+  updateRepoInfo(err, result) {
+    if (err) {
+      throw new Error(err);
+    }
+    if (this.hasChanged) {
+      this.repoInfo = result;
+      this.layout();
+      this.render();
+      this.poll();
+    }
+  }
+  hasChanged(result) {
+    return isEqual(this.repoInfo, result);
   }
   addEventHandlers() {
     this.screen.key(['escape', 'q', 'C-c'], () => {
+      this.screen.destroy();
       process.exit(0);
     });
   }
   layout() {
-    const box = blessed.box({
-      top: 0,
-      left: 0,
-      width: '100%',
-      height: '100%',
-      content: 'Hello {bold}world{/bold}!',
-      tags: true,
+    const list = blessed.list({
+      items: this.repoInfo.branches,
       style: {
-        fg: 'orange',
+        fg: 'blue',
         bg: 'black',
+        transparent: false,
+        scrollbar: {
+          bg: 'blue',
+        },
       },
     });
-    this.screen.append(box);
+    const currentBranch = list.getItemIndex(this.repoInfo.branch);
+    list.select(currentBranch);
+    this.screen.append(list);
   }
 }
 // kick it off
